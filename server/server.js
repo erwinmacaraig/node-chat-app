cba591c6f0192024720fa20b59534a8428419656
Erwin Macaraig <erwin.macaraig@gmail.com>
2017-02-18 01:32:04 +0800

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -12,29 +12,24 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text: 'Hey, what is going on.',
-  //   createdAt: 123
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
 
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
-  // });
-
-  // socket.emit('newMessage', {
-  //   from: 'John',
-  //   text: 'See you then',
-  //   createdAt: 123
-  // });
-
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
-    });
+    });  
   });
 
   socket.on('disconnect', () => {
