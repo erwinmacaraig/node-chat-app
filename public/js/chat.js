7e61906447a82e7d7da96482118d8e5f0d84da4f
Erwin Macaraig <erwin.macaraig@gmail.com>
2017-02-19 17:59:10 +0800

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,8 +77,7 @@ jQuery('#message-form').on('submit', function(e){
   e.preventDefault();
   var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage',{
-    from: 'User',
-    text: messageTextbox.val()
+     text: messageTextbox.val()
   }, function(){
     messageTextbox.val('');
   });
